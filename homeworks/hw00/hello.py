204e50f85c913d88cf89b85a7def40b623001ba0
Баяндин Алексей <bayandin2007leha@mail.ru>
2026-09-14 21:44:45 +0300

    Complete homework 0

---
+++ homeworks/hw00/hello.py
@@ -0,0 +1,6 @@
+print("Hello world")
+import matplotlib
+import numpy
+import pandas
+import platform
+print(platform.python_version(), matplotlib.__version__ , numpy.__version__ , pandas.__version__ )
